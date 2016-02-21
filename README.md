d1587f4e7fd6b82eccd43cc0af70c6e28cf81937
Simon Blackwell <syblackwell@anywhichway.com>
2016-02-21 12:33:18 -0500

    initial public commit

--- README.md
+++ README.md
@@ -1,51 +1,57 @@
-![NPM Downloads](http://img.shields.io/npm/dm/cycle.svg?style=flat) ![NPM Version](http://img.shields.io/npm/v/cycle.svg?style=flat)
+# cycler.js
 
-Fork of https://github.com/douglascrockford/JSON-js, maintained in npm as `cycle`.
+Cycler.js is based on a fork of the hugely popular cycle.js code base from Douglas Crockford, Nuno Job, and Justin Warkentin. 
 
-# Contributors
+Cycler is typically used to serialize objects prior to file storage or network transmission. So long as the same constructors are available in the serializing and de-serializing environments it
+ensures that functional semantics follow along with data as it is transmitted or stored and restored. This is ideal for fully isomorphic programming and distributed processing.
 
-* Douglas Crockford
-* Nuno Job
-* Justin Warkentin
+It enhances their work through:
 
-# JSON in JavaScript
+-- the utilization of newer JavaScript constructs such as Map,
+-- ensuring objects are restored as instances of their original kind rather than just POJOs
+-- providing support for proper handling of classes derived from Array
+-- distinct packaging for NodeJS and the browser including pre-minified code
+-- the addition of unit tests
 
-Douglas Crockford
-douglas@crockford.com
-
-2010-11-18
-
-
-JSON is a light-weight, language independent, data interchange format.
-See http://www.JSON.org/
-
-The files in this collection implement JSON encoders/decoders in JavaScript.
-
-JSON became a built-in feature of JavaScript when the ECMAScript Programming
-Language Standard - Fifth Edition was adopted by the ECMA General Assembly
-in December 2009. Most of the files in this collection are for applications
-that are expected to run in obsolete web browsers. For most purposes, json2.js
-is the best choice.
+[![Build Status](https://travis-ci.org/anywhichway/recycler.svg)](https://travis-ci.org/anywhichway/recycler)
+[![Codacy Badge](https://api.codacy.com/project/badge/grade/42cd44eee8794c22aa7a4f780abd2d0b)](https://www.codacy.com/app/syblackwell/recycler)
+[![Code Climate](https://codeclimate.com/github/anywhichway/recycler/badges/gpa.svg)](https://codeclimate.com/github/anywhichway/recycler)
+[![Test Coverage](https://codeclimate.com/github/anywhichway/recycler/badges/coverage.svg)](https://codeclimate.com/github/anywhichway/recycler/coverage)
+[![Issue Count](https://codeclimate.com/github/anywhichway/recycler/badges/issue_count.svg)](https://codeclimate.com/github/anywhichway/recycler)
 
 
-json2.js: This file creates a JSON property in the global object, if there
-isn't already one, setting its value to an object containing a stringify
-method and a parse method. The parse method uses the eval method to do the
-parsing, guarding it with several regular expressions to defend against
-accidental code execution hazards. On current browsers, this file does nothing,
-prefering the built-in JSON object.
+For reference here is the cycle.js data:
 
-json.js: This file does everything that json2.js does. It also adds a
-toJSONString method and a parseJSON method to Object.prototype. Use of this
-file is not recommended.
+[![NPM](https://nodei.co/npm/cycle.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/cycle/)
 
-json_parse.js: This file contains an alternative JSON parse function that
-uses recursive descent instead of eval.
 
-json_parse_state.js: This files contains an alternative JSON parse function that
-uses a state machine instead of eval.
+# Installation
 
-cycle.js: This file contains two functions, JSON.decycle and JSON.retrocycle,
-which make it possible to encode cyclical structures and dags in JSON, and to
-then recover them. JSONPath is used to represent the links.
-http://GOESSNER.net/articles/JsonPath/
+npm install jovial
+
+The index.js and package.json files are compatible with https://github.com/anywhichway/node-require so that jovial can be served directly to the browser from the node-modules/cycler directory when using node Express.
+
+Browser code can also be found in the browser directory at https://github.com/anywhichway/cycler.
+ 
+# Usage
+
+
+Usage is almost identical to that of cycle.js; with the exception the "root" object is ```Cycler``` rather than ```cycle```.
+
+Additionally, ```Cycler.decycle``` and ```Cycler.retrocycle``` can take 
+a second argument which is the constructor context to be used. If a context is not provided, the currently available scope is used in NodeJS and ```window``` is used in the browser. The only values required in the context are constructors, which should be keyed by their name, e.g.:
+
+```
+function MyConstructor(name) { this.name = name; this.self = this; }; // trivial cyclic class
+var instance = new MyConstructor("joe");
+var decycled = Cycler.decycle(instance,{MyConstructor:MyConstructor});
+Cycler.retrocycle(decycled,{MyConstructor:MyConstructor});
+```
+
+The context is only required for ```.decycle``` if your constructors are anonymous or their name properties are unavailable (as is the case with Internet Explorer).
+
+You will not need to use the context argument for ```.retrocycle```, if all your constructors are available in the same scope as Cycler, .
+
+If constructors are unavailable in a target restoration enviornment, then POJOs will be returned.
+
+
---
+++ browser/cycler.js
@@ -0,0 +1,249 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*
+    Copyright, Feb 2016, AnyWhichWay
+    
+    MIT License (since some CDNs and users must have some type of license and MIT in pretty un-restrictive)
+    
+    Substantive portions based on:
+    
+    cycle.js
+    2013-02-19 crockford
+
+    Public Domain.
+    
+*/
+(function() {
+	"use strict";
+
+var cycler = {};
+
+cycler.decycle = function decycle(object, context) {
+
+// Make a deep copy of an object or array, assuring that there is at most
+// one instance of each object or array in the resulting structure. The
+// duplicate references (which might be forming cycles) are replaced with
+// an object of the form
+//      {$ref: PATH}
+// where the PATH is a JSONPath string that locates the first occurance.
+// So,
+//      var a = [];
+//      a[0] = a;
+//      return JSON.stringify(JSON.decycle(a));
+// produces the string '[{"$ref":"$"}]'.
+
+// JSONPath is used to locate the unique object. $ indicates the top level of
+// the object or array. [NUMBER] or [STRING] indicates a child member or
+// property.
+
+   // AnyWhichWay do any required top-level conversion from POJO's to $kinds
+   context = (context ? context : (this ? this : (typeof(window)!=="undefined" ? window : null)));
+
+   var objects = new Map(); // AnyWhichWay, replaced objects and paths arrays with Map, Feb 2016
+
+    return (function derez(value, path) {
+
+// The derez recurses through the object, producing the deep copy.
+
+        var i,          // The loop counter
+        	pathfound, // AnyWhichWay added Feb 2016
+            name,       // Property name
+            nu;         // The new object or array
+
+// typeof null === 'object', so go on if this value is really an object but not
+// one of the weird builtin objects.
+
+        if (typeof value === 'object' && value !== null &&
+                !(value instanceof Boolean) &&
+                !(value instanceof Date)    &&
+                !(value instanceof Number)  &&
+                !(value instanceof RegExp)  &&
+                !(value instanceof String)) {
+
+// If the value is an object or array, look to see if we have already
+// encountered it. If so, return a $ref/path object. 
+
+            pathfound = objects.get(value); // AnyWhichWay replaced array loops with Map get, Feb 2016
+            if(pathfound) {
+            	return {$ref: pathfound};
+            }
+        	
+// Otherwise, accumulate the unique value and its path.
+			objects.set(value,path); // AnyWhichWay replace array objects and paths with Map, Feb 2016
+// If it is an array, replicate the array.
+            if (Object.prototype.toString.apply(value) === '[object Array]' || value instanceof Array) { // instanceof added by AnyWhichWay, Feb 2016
+                nu = [];
+                for (i = 0; i < value.length; i += 1) {
+                    nu[i] = derez(value[i], path + '[' + i + ']');
+                }
+                // AnyWhichWay augment array with $kind
+                if(!value.constructor.name) {
+                	Object.keys(context).some(function(name) {
+                		if(context[name]===value.constructor) {
+                			nu.push({$kind: name});
+                			return true;
+                		}
+                	});
+                } else {
+                    nu.push({$kind: value.constructor.name});
+                }
+            } else {
+// If it is an object, replicate the object.
+                nu = {};
+                for (name in value) {
+                    if (Object.prototype.hasOwnProperty.call(value, name)) {
+                        nu[name] = derez(value[name],
+                            path + '[' + JSON.stringify(name) + ']');
+                    }
+                }
+                // AnyWhichWay augment objects with $kind
+                if(!value.constructor.name) {
+                	Object.keys(context).some(function(name) {
+                		if(context[name]===value.constructor) {
+                			nu.$kind = name;
+                			return true;
+                		}
+                	});
+                } else {
+                	nu.$kind = value.constructor.name; 
+                }
+            }
+            return nu;
+        }
+        return value;
+    }(object, '$'));
+};
+
+function convert(item) {
+	var obj;
+	if(item && item.$kind) {
+		// make sure constructors exist in target environment
+		if(typeof(this[item.$kind])==="function") {
+          	obj = Object.create(this[item.$kind].prototype);
+       		obj.constructor = this[item.$kind];
+       		for(var key in item) {
+       			if(key!=="$kind") {
+       				obj[key] = item[key];
+       			}
+       		}
+       		item = obj;
+		} else {
+			delete item.$kind;
+		}
+   	// $kind exists as the single data member of the last item in an Array
+   	} else if(item instanceof Array) {
+   		if(item[item.length-1].$kind && Object.keys(item[item.length-1]).length===1) {
+   			if(typeof(this[item[item.length-1].$kind])==="function") {
+           		obj = Object.create(this[item[item.length-1].$kind].prototype);
+           		obj.constructor = this[item[item.length-1].$kind];
+           		for(var i=0;i<item.length-1;i++) {
+           			obj.push(item[i]);
+           		}
+           		item = obj;
+   			} else {
+   				item.splice(item.length-1,1);
+   			}
+   		}
+   	}
+	return item;
+}
+
+cycler.retrocycle = function retrocycle($,context) {
+
+// Restore an object that was reduced by decycle. Members whose values are
+// objects of the form
+//      {$ref: PATH}
+// are replaced with references to the value found by the PATH. This will
+// restore cycles. The object will be mutated.
+
+// AnyWhichWay
+// Object containing $kind member are converted to the kind specified
+// Arrays with last member {$kind: <some kind>} are converted to the specified kind of array
+
+// The eval function is used to locate the values described by a PATH. The
+// root object is kept in a $ variable. A regular expression is used to
+// assure that the PATH is extremely well formed. The regexp contains nested
+// * quantifiers. That has been known to have extremely bad performance
+// problems on some browsers for very long strings. A PATH is expected to be
+// reasonably short. A PATH is allowed to belong to a very restricted subset of
+// Goessner's JSONPath.
+
+// So,
+//      var s = '[{"$ref":"$"}]';
+//      return JSON.retrocycle(JSON.parse(s));
+// produces an array containing a single element which is the array itself.
+	
+	// AnyWhichWay do any required top-level conversion from POJO's to $kinds
+	context = (context ? context : (this ? this : (typeof(window)!=="undefined" ? window : null)));
+	$ = convert.call(context,$);
+	
+    var obj, px =
+        /^\$(?:\[(?:\d+|\"(?:[^\\\"\u0000-\u001f]|\\([\\\"\/bfnrt]|u[0-9a-zA-Z]{4}))*\")\])*$/;
+
+    (function rez(value) {
+
+// Modified by AnyWhichWay, Feb 2016
+// The rez function walks recursively through the object looking for $ref
+// and $kind properties or array values. When it finds a $ref value that is a path, 
+// then it replaces the $ref object with a reference to the value that is found by
+// the path. When it finds a $kind value that names a function in the global scope,
+// it assumes the function is a constructor and uses it to create an object which
+// replaces the JSON such that it is restored with the appropriate class semantics
+// and capability rather than just a general object. If no constructor exists, a
+// POJO is used.
+
+        var i, item, name, path;
+
+        if (value && typeof value === 'object') {
+            if (Object.prototype.toString.apply(value) === '[object Array]' || value instanceof Array) { // AnyWhichWay added instanceof check, Feb 2016
+                for (i = 0; i < value.length; i += 1) {
+                    item = value[i];
+                    if (item && typeof item === 'object') {
+                    	// added by AnyWhichWay, Feb 2016
+                    	item = convert.call(context,item);
+                    	// re-assign in case item has been converted
+                       	value[i] = item;
+                       	// end AnyWhichWay addition
+                        path = item.$ref;
+                        if (typeof path === 'string' && px.test(path)) {
+                            value[i] = eval(path); 
+                        } else {
+                            rez(item);
+                        }
+                    }
+                }
+            } else {
+                for (name in value) {
+                    if (typeof value[name] === 'object') {
+                        item = value[name];
+                        if (item) { 
+                        	// added by AnyWhichWay, Feb 2016
+                        	item = convert.call(context,item);
+                        	// re-assign in case item has been converted
+                           	value[name] = item;
+                            // end AnyWhichWay addition
+                            path = item.$ref;
+                            if (typeof path === 'string' && px.test(path)) {
+                                value[name] = eval(path);
+                            } else {
+                                rez(item);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }($));
+    return $;
+}
+
+if (this.exports) {
+	this.exports  = cycler;
+} else if (typeof define === 'function' && define.amd) {
+	// Publish as AMD module
+	define(function() {return cycler;});
+} else {
+	this.Cycler = cycler;
+}
+
+}).call((typeof(window)!=='undefined' ? window : (typeof(module)!=='undefined' ? module : null)));
+},{}]},{},[1]);
